8e8e10744b48535e6bb666403a5a2976732cd995
tuliooarauj <127243520+tuliooarauj@users.noreply.github.com>
2023-10-18 09:46:01 -0300

    create function sum

--- src/App.java
+++ src/App.java
@@ -1,12 +1,20 @@
 import java.util.Scanner;
 
 public class App {
+
+    public static int AddNumber(int n1, int n2){
+        return n1 + n2;
+    }
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello, World!");
-        System.out.println("Digite algo");
-        String resultado = in.nextLine();
-
-        System.out.println(resultado);
+        
+        System.out.println("Type number 1:");
+        int n1 = in.nextInt();
+        System.out.println("Now, type number 2:");
+        int n2 = in.nextInt();
+        
+        int result = AddNumber(n1, n2);
+        
+        System.out.println("Sum's result: "+ result);
     }
 }
